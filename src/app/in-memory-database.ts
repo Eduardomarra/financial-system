009c1355bdaf8506caadcefa35a71e450252f6da
Eduardo Marra <eduardo.gomes@mpdft.mp.br>
2025-03-18 16:48:19 -0300

    adicionado servico e model de categoria

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,14 +1,15 @@
 import { InMemoryDbService, RequestInfo } from "angular-in-memory-web-api";
+import { Categoria } from "./pages/categorias/shared/categoria.model"
 
 export class InMemoryDatabase extends InMemoryDbService {
 
     createDb(){
-        const categorias = [
-            {id: 1, name: 'Moradia', description: 'Pagamento de contas da casa'},
-            {id: 2, name: 'Saúde', description: 'Plano de saúde e remédios'},
-            {id: 3, name: 'Lazer', description: 'Ciname, parques, praia, etc.'},
-            {id: 4, name: 'Salário', description: 'Recebimento de salário'},
-            {id: 5, name: 'Freelas', description: 'Trabalhos como freelancer'}
+        const categorias: Categoria[] = [
+            {id: 1, nome: 'Moradia', descricao: 'Pagamento de contas da casa'},
+            {id: 2, nome: 'Saúde', descricao: 'Plano de saúde e remédios'},
+            {id: 3, nome: 'Lazer', descricao: 'Ciname, parques, praia, etc.'},
+            {id: 4, nome: 'Salário', descricao: 'Recebimento de salário'},
+            {id: 5, nome: 'Freelas', descricao: 'Trabalhos como freelancer'}
         ];
 
         return {categorias};
