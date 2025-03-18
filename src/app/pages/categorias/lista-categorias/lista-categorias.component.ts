f2650263f81408f1b96277e21bc59ecdb47d91b6
Eduardo Marra <eduardo.gomes@mpdft.mp.br>
2025-03-18 17:14:46 -0300

    retornando dados de lista de categorias

--- src/app/pages/categorias/lista-categorias/lista-categorias.component.ts
+++ src/app/pages/categorias/lista-categorias/lista-categorias.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Categoria} from '../shared/categoria.model'
+import { CategoriaService } from '../shared/categoria.service'
+
 @Component({
   selector: 'app-lista-categorias',
   templateUrl: './lista-categorias.component.html',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaCategoriaComponent implements OnInit {
 
-  constructor() { }
+  categorias: Categoria[] = [];
+
+  constructor(
+    private categoriaService: CategoriaService
+  ) { }
 
   ngOnInit() {
+    this.categoriaService.getAll().subscribe(
+      categorias => this.categorias = categorias,
+      error => console.error('Erro ao carregar categorias:', error)
+    )
+  }
+
+  deletCategoria(categoria: Categoria) {
+    const confirmDelete = confirm("Confirma exclusão da categoria?");
+
+    if(confirmDelete) {
+      this.categoriaService.delete(categoria.id).subscribe(
+        () => {
+          this.categorias = this.categorias.filter(c => c.id!== categoria.id);
+        },
+        error => console.error('Erro ao deletar categoria:', error)
+      )
+    }
   }
 
 }
