9cc74b0003ef2091c45a614aa47b7871231666b8
Eduardo Marra <eduardo.gomes@mpdft.mp.br>
2025-03-26 15:17:39 -0300

    melhoria em entrada service

--- src/app/pages/entradas/shared/entrada.service.ts
+++ src/app/pages/entradas/shared/entrada.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, Injector } from '@angular/core';
 import { Observable } from 'rxjs';
-import { flatMap } from 'rxjs/operators';
+import { catchError, flatMap } from 'rxjs/operators';
 
 import { BaseResourceService } from 'src/app/shared/services/base-resource.service';
 import { CategoriaService } from '../../categorias/shared/categoria.service';
@@ -19,20 +19,20 @@ export class EntradaService extends BaseResourceService<Entrada> {
    } 
 
   create(entrada: Entrada): Observable<Entrada> {
-    return this.categoriaService.getById(entrada.categoriaId).pipe(
-      flatMap(categoria => {
-        entrada.categoria = categoria;
-        return super.create(entrada);
-      })
-    )
+    return this.setCategoriaEEnvioAoServidor(entrada, super.create.bind(this));
   }
 
   update(entrada: Entrada): Observable<Entrada> {
+    return this.setCategoriaEEnvioAoServidor(entrada, super.update.bind(this));
+  }
+
+  private setCategoriaEEnvioAoServidor(entrada: Entrada, envioFn: any): Observable<Entrada> {
     return this.categoriaService.getById(entrada.categoriaId).pipe(
       flatMap(categoria => {
         entrada.categoria = categoria;
-        return super.update(entrada);
-      }))
+        return envioFn(entrada);
+      }),
+      catchError(this.handleError))
   }
 
 }
