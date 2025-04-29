50606073ae413660d9ababf2a9d0c8583e6973fa
Eduardo Marra <eduardo.gomes@mpdft.mp.br>
2025-04-29 15:56:55 -0300

    ajuste na rota para relatórios como pagina inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
+  {path: '', redirectTo: 'relatorios', pathMatch: 'full'},
   {path: 'categorias', loadChildren: './pages/categorias/categorias.module#CategoriasModule'},
   {path: 'lancamentos', loadChildren: './pages/entradas/entradas.module#EntradasModule'},
   {path: 'relatorios', loadChildren: './pages/relatorios/relatorios.module#RelatoriosModule'},
